0e07af7c7d9a5930b1a5f0acc59063a6f873969e
Mark Culhane <mark.culhane@assetowl.com>
2016-04-24 16:33:46 +1000

    fixing youtube-dler

--- scripts/random/GetYoutubePlaylist.sh
+++ scripts/random/GetYoutubePlaylist.sh
@@ -1,9 +1,13 @@
 # Simple script to dl and convert youtube playlists - MarkC, 2013
+
+# Prereqs - 
+#		youtube-dl --> https://rg3.github.io/youtube-dl/download.html
+#		avconv --> https://libav.org/download/
+# on osx simply:
+#				brew install youtube-dl libav
+
 # Usage: sh GetYoutubePlaylist.sh [youtube playlist addess] [output directory]
+
 # To get the youtube playlist address, view the playlist then click on share, copy that URL
 # example: sh GetYouTubePlaylist.sh http://www.youtube.com/playlist?list=PL702CAF4AD2AED35B /home/lp1/Music/youtube
-# ytdl - abritrary extension name
-outputFileName="/%(playlist)s/%(title)s"
-youtube-dl -o "$2%(playlist)s/%(title)s.ytdl" --max-quality url $1
-find $2 -type f -name "*.ytdl" -exec avconv -i '{}' '{}'.mp3 ;
-find $2 -name *.ytdl -exec rm {} +
+youtube-dl --extract-audio --audio-format mp3  --audio-quality 0 -o "$2%(playlist)s/%(title)s-%(id)s.%(ext)s." $1 --yes-playlist
